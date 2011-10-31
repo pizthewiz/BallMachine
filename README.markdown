a270a46e4ead9b98e120e12b8eb6453ba50a48ae
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-10-31 00:55:14 -0700

    add basic composition loading and playback
    syphon render does not appear to work properly yet, though i don't quite know why. a runloop is active as network operations are succeeding, but perhaps the off-screen renderer is not all it is cracked up to be.

--- README.markdown
+++ README.markdown
@@ -1,3 +1,7 @@
 
 # Ball Machine
-a small command-line tool to load and optionally render out a Quartz Composer composition
+a small command-line tool to load and run a Quartz Composer composition off-screen
+
+### THANKS
+- Anton Marini (vade) for [Syphon](http://syphon.v002.info/) and perpetual help even when i don't deserve it
+- Mike Ash for his timing hints from [MABGTimer.m](https://github.com/mikeash/MABGTimer/blob/master/Source/MABGTimer.m)
--- ballmachine/main.m
+++ ballmachine/main.m
@@ -8,15 +8,138 @@
 
 #import <Foundation/Foundation.h>
 #import <Quartz/Quartz.h>
+#import <mach/mach_time.h>
 
-@interface RenderSlave : NSObject <NSApplicationDelegate> {}
+#ifdef DEBUG
+    #define CCDebugLogSelector() NSLog(@"-[%@ %@]", /*NSStringFromClass([self class])*/self, NSStringFromSelector(_cmd))
+    #define CCDebugLog(a...) NSLog(a)
+    #define CCWarningLog(a...) NSLog(a)
+    #define CCErrorLog(a...) NSLog(a)
+#else
+    #define CCDebugLogSelector()
+    #define CCDebugLog(a...)
+    #define CCWarningLog(a...) NSLog(a)
+    #define CCErrorLog(a...) NSLog(a)
+#endif
+
+// taken from MABGTimer https://github.com/mikeash/MABGTimer/blob/master/Source/MABGTimer.m
+static NSTimeInterval _Now(void) {
+    static mach_timebase_info_data_t info;
+    static dispatch_once_t pred;
+    dispatch_once(&pred, ^{
+        mach_timebase_info(&info);
+    });
+
+    NSTimeInterval t = mach_absolute_time();
+    t *= info.numer;
+    t /= info.denom;
+    return t;
+}
+
+// based on a combo of watchdog timer and MABGTimer
+//  http://www.fieryrobot.com/blog/2010/07/10/a-watchdog-timer-in-gcd/
+//  http://www.mikeash.com/pyblog/friday-qa-2010-07-02-background-timers.html
+@interface RenderTimer : NSObject {
+    dispatch_source_t _timer;
+    dispatch_queue_t _queue;
+}
+- (id)initWithInterval:(NSTimeInterval)interval do:(void (^)(void))block;
+- (void)cancel;
+@end;
+@implementation RenderTimer
+- (id)initWithInterval:(NSTimeInterval)interval do:(void (^)(void))block {
+    self = [super init];
+    if (self) {
+        // run on main queue for runloop love
+        _queue = dispatch_get_main_queue();
+        _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, _queue);
+        // NB - this fires after interval, not immediately
+        dispatch_source_set_timer(_timer, dispatch_time(DISPATCH_TIME_NOW, 0), interval * NSEC_PER_SEC, 0);
+        dispatch_source_set_event_handler(_timer, ^{
+            block();
+        });
+        dispatch_resume(_timer);
+    }
+    return self;
+}
+- (void)dealloc {
+    [self cancel];
+}
+- (void)cancel {
+    dispatch_sync(_queue, ^{
+        dispatch_source_cancel(_timer);
+        dispatch_release(_timer);
+        _timer = NULL;
+    });
+}
+@end
+
+
+@interface RenderSlave : NSObject <NSApplicationDelegate>
+@property (nonatomic, strong) QCRenderer* renderer;
+@property (nonatomic, strong) RenderTimer* renderTimer;
+@property (nonatomic) NSTimeInterval startTime;
 - (void)loadCompositionAtURL:(NSURL*)location;
+- (void)startRendering;
+- (void)stopRendering;
+- (void)_render;
 @end
 
 @implementation RenderSlave
-- (void)loadCompositionAtURL:(NSURL*)location {
-    NSLog(@"setup");
+
+@synthesize renderer, renderTimer, startTime;
+
+- (void)dealloc {
+    [self stopRendering];
 }
+
+- (void)loadCompositionAtURL:(NSURL*)location {
+    CCDebugLogSelector();
+
+    QCComposition* composition = [QCComposition compositionWithFile:location.path];
+    if (!composition) {
+        CCErrorLog(@"ERROR - failed to create composition from path '%@'", location.path);
+        exit(0);
+    }
+
+    // TODO - make size a settable value
+    self.renderer = [[QCRenderer alloc] initOffScreenWithSize:NSMakeSize(1280, 720) colorSpace:NULL composition:composition];
+    if (!self.renderer) {
+        CCErrorLog(@"ERROR - failed to create renderer for composition %@", composition);
+        exit(0);
+    }
+}
+
+- (void)startRendering {
+    CCDebugLogSelector();
+
+    self.renderTimer = [[RenderTimer alloc] initWithInterval:(1./60.) do:^{
+        [self _render];
+    }];
+}
+
+- (void)stopRendering {
+    CCDebugLogSelector();
+
+    self.renderTimer = nil;
+    self.startTime = 0;
+}
+
+- (void)_render {
+    CCDebugLogSelector();
+
+    NSTimeInterval now = _Now();
+    if (!startTime)
+        startTime = now;
+    NSTimeInterval relativeTime = (now - startTime) / 1000.;
+    NSTimeInterval nextRenderTime = [renderer renderingTimeForTime:relativeTime arguments:nil];
+//    CCDebugLog(@"now:%f relativeTime:%f nextRenderTime:%f", now, relativeTime, nextRenderTime);
+    if (relativeTime >= nextRenderTime) {
+        CCDebugLog(@"\trendering");
+        [renderer renderAtTime:relativeTime arguments:nil];
+    }
+}
+
 @end
 
 #pragma mark -
@@ -24,7 +147,7 @@
 int main (int argc, const char * argv[]) {
     @autoreleasepool {
         if (argc != 2) {
-            NSLog(@"usage: %@ <source-composition> <additional-plugin-directory>", [[NSString stringWithUTF8String:argv[0]] lastPathComponent]);
+            CCErrorLog(@"usage: %@ <composition>", [[NSString stringWithUTF8String:argv[0]] lastPathComponent]);
             return -1;
         }
 
@@ -35,26 +158,30 @@ int main (int argc, const char * argv[]) {
             if ([path isAbsolutePath]) {
                 url = [NSURL fileURLWithPath:path isDirectory:NO];
             } else {
-//                NSURL* baseDirectoryURL = [[context compositionURL] URLByDeletingLastPathComponent];
-//                url = [baseDirectoryURL URLByAppendingPathComponent:path];
+                // relative path
+                NSString* currentDirectoryPath = [[NSFileManager defaultManager] currentDirectoryPath];
+                NSString* resolvedFilePath = [currentDirectoryPath stringByAppendingPathComponent:[filePath stringByStandardizingPath]];
+                url = [NSURL fileURLWithPath:resolvedFilePath isDirectory:NO];
             }
         }
         // double check
         if (![url isFileURL]) {
-            NSLog(@"ERROR - filed to create URL for path '%@'", filePath);
+            CCErrorLog(@"ERROR - filed to create URL for path '%@'", filePath);
             return -1;
         }
 
         NSError* error;
         if (![url checkResourceIsReachableAndReturnError:&error]) {
-            NSLog(@"ERROR - bad source composition URL: %@", [error localizedDescription]);
+            CCErrorLog(@"ERROR - bad source composition URL: %@", [error localizedDescription]);
             return -1;
         }
 
         RenderSlave* renderSlave = [[RenderSlave alloc] init];
         [renderSlave loadCompositionAtURL:url];
+        [renderSlave startRendering];
 
-        [[NSRunLoop currentRunLoop] run];
+//        [[NSRunLoop currentRunLoop] run];
+        CFRunLoopRun();
     }
     return 0;
 }
